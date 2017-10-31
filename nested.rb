1be250228a57ffb1f0850e6151d006549bea5419
Matt Faircloth <mattcfaircloth@gmail.com>
2017-10-31 02:42:10 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -81,8 +81,8 @@ def adding_matz
           :languages => ["C"]
         }
      }
- programmer_hash[:yukihiro_matsumoto] = {:known_for => "Ruby"}
- 
+ programmer_hash[:yukihiro_matsumoto][:known_for] = "Ruby"
+
 end
 
 def changing_alan
